3e110a34299dd5487c29176e1041e871f24080a3
Scott Kennedy <scottyzen@gmail.com>
2018-08-17 14:58:03 +0100

    Added dummy images and fixed thumbnail toggle

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -8,7 +8,9 @@
             <img src="~/assets/images/pub-front-snow.jpg" alt="O' Driscolls bar cover in snow durning the winter of 2018">
         </picture>
     </div>
-    <div class="container container--style p-10 md:p-12">
+
+    <!-- Gallery -->
+    <div class="container container--style container--bottom-gap p-10 md:p-12">
         <div class="row flex flex-wrap">
             <div class="text-center w-full mb-8 lg:mb-16">
                 <h2 class="text-underline">Gallery</h2>
@@ -22,8 +24,14 @@
                 <div class="text-grey-dark">
                     <div class="type w-full">
                         <p class="font-bold text-xs text-orange uppercase">View mode</p>
-                        <p class="m-0">Full images</p>
-                        <p class="m-0">Thumbnails</p>
+                        <p class="m-0">
+                            <a @click="thumbnailView = false" :class="{'font-bold' : !thumbnailView}">Full images</a>
+                        </p>
+                        <p class="m-0">
+                            <a @click="thumbnailView = true" :class="{'font-bold' : thumbnailView}">Thumbnails</a>
+                        </p>
+                        
+                        
                     </div>
                     <br>
                     <div class="type w-full">
@@ -50,10 +58,23 @@
                 :key="imageIndex"
                 @click="index = imageIndex"
                 :style="{ backgroundImage: 'url(' + image + ')' }"
+                :class="{'thumbnailView': thumbnailView}"
                 ><span>{{image.title}}</span></div>
             </div>
         </div>
+    </div><!-- Gallery -->
+
+    <div class="container container--style">
+        <div class="row">
+            <div class="text-center">
+                <div class="text-underline">
+                    <h3>#odriscollsbar</h3>
+                </div>
+
+            </div>
+        </div>
     </div>
+
   </div>
 </template>
 <script>
@@ -62,7 +83,8 @@ export default {
     data () {
       return {
         images: galleryImages,
-        index: null
+        index: null,
+        thumbnailView: true
       };
     }
 }
@@ -72,9 +94,17 @@ export default {
     background-size: cover
     background-repeat: no-repeat
     background-position: center center
-    min-height: 150px
+    min-height: 15rem
     border: $border
     width: 100%
+    +tablet
+        min-height: 20rem
+    +laptop
+        min-height: 25rem
+            
+
+.thumbnailView
+    min-height: 150px
     +tablet
         width: 47.5%
     +laptop
