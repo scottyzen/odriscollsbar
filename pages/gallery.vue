28443367bf3094a5b37797bb5f87f830dddeafbd
Scott Kennedy <scottyzen@gmail.com>
2018-09-20 01:25:04 +0100

    gallery fix

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -86,31 +86,17 @@ export default {
     data () {
       return {
         images: [
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t1.0-9/39008928_1824819147611376_6330601676260507648_o.jpg?_nc_cat=0&oh=609d82072bea8b905a735bcd6f17c141&oe=5BECC843',
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t1.0-9/37211960_1778924895534135_6118067199597871104_o.jpg?_nc_cat=0&oh=ab1607669b815e83c0467db955adbc07&oe=5BEDF3C6',
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t1.0-9/37173676_1778924608867497_1652228143009759232_n.jpg?_nc_cat=0&oh=50e493460163e64babac267f7a79a416&oe=5BFAB732',
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t1.0-9/37199434_1778919512201340_8598332408409882624_n.jpg?_nc_cat=0&oh=910154911817f3a99bf2e2fbbb02aaae&oe=5BF4628C',
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t1.0-9/37127949_1778916625534962_6598511076076158976_n.jpg?_nc_cat=0&oh=d30068d593093a5747a3d91694ac49af&oe=5BFC81A5',
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t1.0-9/37154558_1778917345534890_3677751757938622464_o.jpg?_nc_cat=0&oh=5cc398c35567064c96c1db643ca16943&oe=5C1057F7',
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t1.0-9/37161642_1778915098868448_8378884473214730240_o.jpg?_nc_cat=0&oh=9f925e725a34fc26294de63738164717&oe=5C0278C5',
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t1.0-9/36047714_1745967968829828_2162744074824581120_o.jpg?_nc_cat=0&oh=749d1bd1f1e615c9fc5e223e7c1db8d1&oe=5BF29DB9',
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t1.0-9/35546592_1745967078829917_5418480261882970112_o.jpg?_nc_cat=0&oh=03e4ff7bedd1a1acdec140be0b14c7b8&oe=5BF0E01C',
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t1.0-9/36177418_1745964842163474_5990278247366000640_n.jpg?_nc_cat=0&oh=a056f021dcc6e20269ae066687395527&oe=5C0E63E1',
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t1.0-9/34536459_1721726877920604_4505237869631111168_n.jpg?_nc_cat=0&oh=a57c4eb97b4b6ab54fb03dd3299d79d1&oe=5C0CC813',
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t1.0-9/34530419_1721725114587447_6175814044209905664_n.jpg?_nc_cat=0&oh=c2073492ed0ad66cca0403e899f8cb56&oe=5C0D1AC2',
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t1.0-9/34746423_1721714927921799_5957957939858767872_n.jpg?_nc_cat=0&oh=4020ab5c8eac72c26be760e1e83b2a2e&oe=5BED1D68',
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t1.0-9/34699461_1721704057922886_1319632412547743744_n.jpg?_nc_cat=0&oh=b1d604a2d6e35bfd495f368a8dcf57dc&oe=5C077FA2',
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t1.0-9/34512506_1721689887924303_1128469899308433408_n.jpg?_nc_cat=0&oh=b6f64de2fe5e2013a45bb96d13990a12&oe=5C0BA32C',
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t1.0-9/34507502_1721681974591761_1271472231293124608_o.jpg?_nc_cat=0&oh=740d489d84379210deaecc893b9ebd17&oe=5C01B230',
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t1.0-9/34502218_1721685267924765_2723419160162861056_o.jpg?_nc_cat=0&oh=bb34a9872dc764cbcb4c2cf65594d5ec&oe=5C046C25',
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t1.0-9/34535880_1721656654594293_7691902311030849536_n.jpg?_nc_cat=0&oh=bbf1938d9d60a8e26c4f7bca05e48d0c&oe=5C0C01D6',
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t1.0-9/34469333_1720067894753169_6176279421096296448_n.jpg?_nc_cat=0&oh=18d096c9f0ea7001fd5c508095a16f66&oe=5C0297DB',
-            'https://scontent.fdub2-1.fna.fbcdn.net/v/t31.0-8/28514670_1625533234206636_8335570396976788223_o.jpg?_nc_cat=0&oh=94e1b267810330dbdf21a58a86149848&oe=5BFE1935',
             {
                 title: 'Candle',
-                href: '~/assets/images/candle-large.jpg',
+                href: ' https://fillmurray.com/g/600/800',
                 type: 'image/jpeg',
-                thumbnail: '~/assets/images/candle.jpg'
+                thumbnail: ' https://fillmurray.com/g/200/300'
+            },
+            {
+                title: 'Candle',
+                href: ' https://fillmurray.com/g/400/600',
+                type: 'image/jpeg',
+                thumbnail: ' https://fillmurray.com/g/200/300'
             }
         ],
         index: null,
