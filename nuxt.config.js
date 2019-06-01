f541b12695d9357ec4823282878388824b1146de
Scott Kennedy <scottyzen@gmail.com>
2019-06-01 16:31:06 +0100

    Added expire headers

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,8 @@ module.exports = {
 	// Server side rendering turned on
 	mode: 'universal',
 
+	version: pkg.version,
+
 	// Headers of the page
 	head: {
 		title: "O' Driscolls Bar",
@@ -67,11 +69,47 @@ module.exports = {
 
 	// Modules
 	modules: [
-		'nuxt-sass-resources-loader'
+		'nuxt-sass-resources-loader',
+		'nuxt-ssr-cache'
 	],
 	sassResources: [
 		'@/assets/sass/main.sass'
 	],
+
+	// Cache middleware for nuxt's SSR rendering.
+	cache: {
+		// if you're serving multiple host names (with differing
+		// results) from the same server, set this option to true.
+		// (cache keys will be prefixed by your host name)
+		// if your server is behind a reverse-proxy, please use
+		// express or whatever else that uses 'X-Forwarded-Host'
+		// header field to provide req.hostname (actual host name)
+		useHostPrefix: false,
+
+		store: {
+			type: 'memory',
+
+			// maximum number of pages to store in memory
+			// if limit is reached, least recently used page
+			// is removed.
+			max: 100,
+
+			// number of seconds to store this page in cache
+			ttl: 60,
+		},
+		pages: [
+			// these are prefixes of pages that need to be cached
+			// if you want to cache all pages, just include '/'
+			'/page1',
+			'/page2',
+
+			// you can also pass a regular expression to test a path
+			/^\/page3\/\d+$/,
+
+			// to cache only root route, use a regular expression
+			/^\/$/
+		],
+	},
 
 	// Build configuration
 	build: {
